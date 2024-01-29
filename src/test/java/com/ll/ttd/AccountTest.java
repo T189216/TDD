51436f8dca7d12c12a21399080ee4955b7021915
mhm301 <tus0926@naver.com>
2024-01-29 11:55:03 +0900

    1

---
+++ src/test/java/com/ll/ttd/AccountTest.java
@@ -0,0 +1,13 @@
+package com.ll.ttd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTest {
+
+    @Test
+    void testAccount() {
+        Account account = new Account();
+    }
+}
