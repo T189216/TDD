6d551f3a4f6fd54b81caca23839391dd0f948757
seoyeon <tus0926@naver.com>
2024-01-30 10:20:29 +0900

    Refine 3

--- src/main/java/com/ll/ttd/Account.java
+++ src/main/java/com/ll/ttd/Account.java
@@ -1,10 +1,12 @@
 package com.ll.ttd;
 
 public class Account {
+    private int balance;
     public Account(int i) {
+        this.balance = i;
     }
 
     public int getBalance() {
-        return 10000;
+        return this.balance;
     }
 }
--- src/test/java/com/ll/ttd/AccountTest.java
+++ src/test/java/com/ll/ttd/AccountTest.java
@@ -29,6 +29,16 @@ class AccountTest {
         Account account = new Account(10000);
 
         if (account.getBalance() != 10000) {
+            fail("getBalance() => " + account.getBalance() );
+        }
+
+        account = new Account(1000);
+        if (account.getBalance() != 1000) {
+            fail();
+        }
+
+        account = new Account(0);
+        if (account.getBalance() != 0) {
             fail();
         }
     }
